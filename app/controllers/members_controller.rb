ebf1913275c645c37208e1c1fb24dbe15ddf8117
Shubham <shubham@squadrun.co>
2016-09-18 18:40:26 -0400

    Delete Functionality added

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,12 +1,23 @@
 class MembersController < ApplicationController
+  before_action :set_member, only: [:destroy, :show]
 
   def index
     @members = User.where.not(is_admin: true)
   end
 
   def show
-    puts params
-    @member = User.find(params[:id])
+    @booking_history = Reservation.where(user: params[:id])
   end
+
+  def destroy
+    @member.destroy
+    redirect_to action: 'index'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_member
+      @member = User.find(params[:id])
+    end
 
 end
