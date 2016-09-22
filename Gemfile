cd9699035709cb82cc401fc9309ddbb32db311a4
keemen90 <akeemeagles@gmail.com>
2016-09-21 22:59:42 -0400

    Added initial admin. This fix #17

--- Gemfile
+++ Gemfile
@@ -50,3 +50,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'coffee-script-source', '1.8.0'
 gem 'devise', '~> 4.2'
 gem 'wdm', '>= 0.1.0' if Gem.win_platform?
+gem 'bcrypt-ruby', '3.1.1.rc1', :require => 'bcrypt'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+admin1 = Member.create :name => "admin", :email => "admin@mail.com", :password => "password", :is_admin => true
