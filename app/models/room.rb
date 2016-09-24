25f041b6f733fabc9d45dba57705f626c0eee62f
keemen90 <akeemeagles@gmail.com>
2016-09-24 17:35:42 -0400

    Updated the members and room models, added the new view for members, and updated the route for members.

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,4 +3,7 @@ class Room < ApplicationRecord
   validates_inclusion_of :size, in: Choices['size']
   validates_inclusion_of :status, in: Choices['status']
   validates :room_number, uniqueness:true
+
+  has_many :reservations
+  belongs_to :members
 end
