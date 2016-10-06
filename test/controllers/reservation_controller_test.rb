ce7609920f1a098bb146d865a851f7b1a9b76565
keemen90 <akeemeagles@gmail.com>
2016-10-05 21:36:58 -0400

    Forgot to add name to edit member page

--- test/controllers/reservation_controller_test.rb
+++ test/controllers/reservation_controller_test.rb
@@ -1,5 +1,25 @@
 require 'test_helper'
 
 class ReservationControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @reservation = reservations(:one)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create reservation" do
+    assert_difference('Reservation.count') do
+      post :create, reservation: {room_id: @reservation.room_id, member_id: @reservation.member_id, starttime: @reservation.starttime}
+    end
+    assert_redirected_to reservations_path(assigns(:reservation))
+  end
 
 end
